6289ea8727df468034eb7821b68c65f1531456bb
Enoch Kumala <daneolog@gmail.com>
2020-05-08 17:23:03 -0400

    login and register are working!

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -18,13 +18,25 @@ class User(Base):
     zipcode = Column(String)
     fname = Column(String)
     lname = Column(String)
-    num_transactions = Column(String)
+    num_transactions = Column(Integer)
     rating = Column(Integer)
 
-    def __init__(self):
-        username = ''
+    def __init__(self, username, password, email, phone='phone', picture='picture', street='street', city='city', state='state', zipcode='zipco', fname='fname', lname='lname', num_transactions=0, rating=5):
+        self.username = username
+        self.password = password
+        self.email = email
+        self.phone = phone
+        self.picture = picture
+        self.street = street
+        self.city = city
+        self.state = state
+        self.zipcode = zipcode
+        self.fname = fname
+        self.lname = lname
+        self.num_transactions = num_transactions
+        self.rating = rating
 
     def __str__(self):
-        return f'{self.fname} {self.lname}'
+        return f'{self.fname} {self.lname}: {self.username}'
 
 # class idk
--- backend/src/register.py
+++ backend/src/register.py
@@ -34,8 +34,8 @@ class RegisterUser(Resource):
             password = args.get('password')
             pwd_hash = bcrypt.generate_password_hash(password)
 
-            user = User(fname=args.get('fname'), lname=args.get(
-                'lname'), username=username, password=pwd_hash, email=args.get('email'))
+            user = User(username=username, password=pwd_hash, email=args.get('email'))
+            print(user)
             session.add(user)
             session.commit()
 
