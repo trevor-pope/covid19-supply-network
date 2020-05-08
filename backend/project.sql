d51fcdfe83bbcfef927a2c7dbe653994e7a5453f
Enoch Kumala <daneolog@gmail.com>
2020-05-08 16:49:30 -0400

    updated SQL file

--- backend/project.sql
+++ backend/project.sql
@@ -18,6 +18,35 @@ USE `covid_supply`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `offers`
+--
+
+DROP TABLE IF EXISTS `offers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `offers` (
+  `offerId` int NOT NULL,
+  `user_email` varchar(64) DEFAULT NULL,
+  `quantity` float DEFAULT NULL,
+  `item` varchar(32) DEFAULT NULL,
+  `price` float DEFAULT NULL,
+  `willing_to_transport` tinyint DEFAULT NULL,
+  PRIMARY KEY (`offerId`),
+  KEY `fk_email_idx` (`user_email`),
+  CONSTRAINT `fk_offer_email` FOREIGN KEY (`user_email`) REFERENCES `user` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `offers`
+--
+
+LOCK TABLES `offers` WRITE;
+/*!40000 ALTER TABLE `offers` DISABLE KEYS */;
+/*!40000 ALTER TABLE `offers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `requests`
 --
 
@@ -26,16 +55,15 @@ DROP TABLE IF EXISTS `requests`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `requests` (
   `requestId` int NOT NULL,
-  `userEmail` varchar(64) DEFAULT NULL,
-  `minQuantity` float DEFAULT NULL,
+  `user_email` varchar(64) DEFAULT NULL,
+  `min_quantity` float DEFAULT NULL,
   `quantity` float DEFAULT NULL,
   `urgency` smallint DEFAULT NULL,
   `item` varchar(32) DEFAULT NULL,
   `fulfilled` tinyint DEFAULT NULL,
-  `requestscol` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`requestId`),
-  KEY `fk_email_idx` (`userEmail`),
-  CONSTRAINT `fk_email` FOREIGN KEY (`userEmail`) REFERENCES `user` (`email`) ON DELETE CASCADE ON UPDATE CASCADE
+  KEY `fk_email_idx` (`user_email`),
+  CONSTRAINT `fk_requests_email` FOREIGN KEY (`user_email`) REFERENCES `user` (`email`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -46,6 +74,66 @@ CREATE TABLE `requests` (
 LOCK TABLES `requests` WRITE;
 /*!40000 ALTER TABLE `requests` DISABLE KEYS */;
 /*!40000 ALTER TABLE `requests` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `review`
+--
+
+DROP TABLE IF EXISTS `review`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `review` (
+  `reviewId` int NOT NULL,
+  `transactionId` int DEFAULT NULL,
+  `is_supplier` tinyint DEFAULT NULL,
+  `score` int DEFAULT NULL,
+  `desc` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`reviewId`),
+  KEY `fk_transactions_idx` (`transactionId`),
+  CONSTRAINT `fk_transactions` FOREIGN KEY (`transactionId`) REFERENCES `transactions` (`transactionId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `review`
+--
+
+LOCK TABLES `review` WRITE;
+/*!40000 ALTER TABLE `review` DISABLE KEYS */;
+/*!40000 ALTER TABLE `review` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `transactions`
+--
+
+DROP TABLE IF EXISTS `transactions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `transactions` (
+  `transactionId` int NOT NULL,
+  `offerId` int DEFAULT NULL,
+  `requestId` int DEFAULT NULL,
+  `quantity_fulfilled` varchar(45) DEFAULT NULL,
+  `transaction_date` varchar(45) DEFAULT NULL,
+  `final_cost` varchar(45) DEFAULT NULL,
+  `delivery_type` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`transactionId`),
+  KEY `fk_transactions_request_idx` (`requestId`),
+  KEY `fk_transactions_offer_idx` (`offerId`),
+  CONSTRAINT `fk_transactions_offer` FOREIGN KEY (`offerId`) REFERENCES `offers` (`offerId`),
+  CONSTRAINT `fk_transactions_request` FOREIGN KEY (`requestId`) REFERENCES `requests` (`requestId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `transactions`
+--
+
+LOCK TABLES `transactions` WRITE;
+/*!40000 ALTER TABLE `transactions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `transactions` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -91,4 +179,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-05-08 16:28:58
+-- Dump completed on 2020-05-08 16:49:09
