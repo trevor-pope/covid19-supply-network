9a0b6c8d73a8f11c5e37f470e6d9464fe0534c4a
michaelsaia <michaelsaia97@gmail.com>
2020-05-09 22:37:57 -0400

    offer namespace added

--- backend/app.py
+++ backend/app.py
@@ -7,10 +7,12 @@ api = Api(app)
 from src.auth import api as api_auth
 from src.request import api as api_request
 from src.review import api as api_review
+from src.offer import api as api_offer
 
 api.add_namespace(api_auth)
 api.add_namespace(api_request)
 api.add_namespace(api_review)
+api.add_namespace(api_offer)
 
 if __name__ == '__main__':
     app.run(debug=True)
